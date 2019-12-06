4932fdb16c169f819968840bde8d899e56642270
Tony Josi <Tony.Josi@analog.com>
2019-12-06 12:21:27 +0530

    edits

--- new.py
+++ new.py
@@ -1,9 +1,6 @@
-print "HELLO WORLD -- Master"
-<<<<<<< HEAD
-print "from master added after change in issue#1"
-print "change from new folder 2"
-||||||| merged common ancestors
-=======
+print ("HELLO WORLD -- Master")
+print ("from master added after change in issue#1")
+print ("change from new folder 2")
 
 
 
@@ -12,5 +9,5 @@ print "change from new folder 2"
 
 
 
-print "from issue#1"
->>>>>>> issue#1
+print ("from issue#1")
+print ("after merge")
