0fef2b257d7127053e852f628281215be424df65
Tony Josi <Tony.Josi@analog.com>
2020-02-12 10:35:03 +0530

    testing cherry-pic

--- isuue2.py
+++ isuue2.py
@@ -6,3 +6,5 @@ update added to remote before fetch
 after adding repo as sub - module
 
 change from tutorial inside issue2 branch
+
+this should be in master branch ==> for testing cherry-pic feature of git
